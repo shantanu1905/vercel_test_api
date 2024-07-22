4660b9b10932be7ac51305d742266ea3dd5b01d9
shantanu1905 <nimkarshantanu07@gmail.com>
2024-07-22 23:24:37 +0530

    templates removed

--- main.py
+++ main.py
@@ -13,31 +13,6 @@ app.include_router(auth.router)
 
 
 
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.get("/signup", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("create_user.html", {"request": request})
-
-@app.get("/activate_account", name="activate_account",response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("generate_otp.html", {"request": request})
-
-@app.get("/verify_account", name="verify_account",response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("verify_account.html", {"request": request})
-
-@app.get("/login", name="login",response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("login.html", {"request": request})
-
-@app.get("/profile", name="profile",response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("profile.html", {"request": request})
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0")
